8708d6f8c5592e7d6689acdf713eb492566fdc77
qcamiloq <qcamiloq@gmail.com>
2016-11-25 10:56:20 -0500

    Migration js to an external file

--- js/script.js
+++ js/script.js
@@ -17,3 +17,26 @@ $(window).resize(function(){
 	
 });
 
+$(document).ready(function(){
+			
+		    $('a[href^="#"]').on('click',function (e) {
+		        e.preventDefault();
+		        var target = this.hash;
+		        
+		        $target = $(target);
+		        $('html, body').stop().animate({
+		            'scrollTop':  $target.offset().top - 50//no need of parseInt here
+		        }, 1000, 'swing', function () {
+		            window.location.hash = target ;
+		        });
+		    });
+});	
+
+$('.navbar li').click(function(e) {
+		    $('.navbar li.active').removeClass('active');
+		    var $this = $(this);
+		    if (!$this.hasClass('active')) {
+		        $this.addClass('active');
+		    }
+	    	e.preventDefault();
+		});
